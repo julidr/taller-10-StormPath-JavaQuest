870ed321b1c38bd186ef289e075c3230a5139df7
Juliana Diaz <juli.milena@hotmail.com>
2017-03-31 19:56:35 -0500

    Arreglo de Interfaz, creacionde usuario y login

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var stormpath = require('express-stormpath');
-
+var path = require('path');
+var routes = require('./routes/index');
 var app = express();
+
+//view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(stormpath.init(app, {
   // Optional configuration options.
@@ -9,6 +15,8 @@ app.use(stormpath.init(app, {
     href: `https://api.stormpath.com/v1/applications/1IhAOqoI27b9JSAmMFxhTq`
   }
 }));
+
+app.use('/', routes);
 
 app.on('stormpath.ready', function () {
   console.log('Stormpath Ready!');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var stormpath = require('express-stormpath');
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', stormpath.getUser, function(req, res, next) {
+  res.render('index', { title: 'Stormpath SignUp' });
+});
+
+router.get('/profile', stormpath.authenticationRequired, function(req, res) {
+  res.render('profile');
+});
+
+router.post('/profile', bodyParser.urlencoded({extended: false}), stormpath.authenticationRequired, function(req, res, next) {
+  for (var key in req.body) {
+     console.log("Entre a PROFILE");
+    req.user.customData[key] = req.body[key];
+  }
+
+  req.user.customData.save(function(err) {
+    if (err) {
+      return next(err);
+    }
+    res.render('profile');
+  });
 });
 
 module.exports = router;
